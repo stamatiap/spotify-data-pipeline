13c9327d80b2339ad7982ce7a93574d29349e19c
stamatiap <s240146@dtu.dk>
2025-08-30 14:52:14 +0200

    Add marts for artist and track popularity

---
+++ spotify_data_pipeline/models/mart/mart_track_owner_popularity.sql
@@ -0,0 +1,21 @@
+WITH playlist_tracks AS (
+
+    SELECT * 
+    FROM {{ ref('map_playlist_tracks') }}
+
+),
+
+playlist_owners AS (
+
+    SELECT * 
+    FROM {{ ref('dim_playlist_cleansed') }}
+
+),
+
+SELECT
+    pt.track_id,
+    COUNT(DISTINCT p.owner_id) AS owner_count
+FROM playlist_tracks AS pt
+LEFT JOIN playlist_owners AS p
+    ON p.playlist_id = pt.playlist_id
+GROUP BY pt.track_id
---
+++ spotify_data_pipeline/models/mart/mart_track_playlist_populairty.sql
@@ -0,0 +1,8 @@
+WITH playlist_tracks AS (
+    SELECT * FROM {{ref("map_playlist_tracks")}}
+)
+
+SELECT track_id, 
+    COUNT( DISTINCT playlist_id) AS playlist_count
+FROM playlist_tracks
+GROUP BY track_id
